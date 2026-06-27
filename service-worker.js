5726efbada59c3dc667c23383d7d864b8be0a96d
douglasjoseluciano79-byte <douglasjoseluciano79@gmail.com>
2026-06-27 00:52:05 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "alquran-v2";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/surata.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/js/surata.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request)
+        .then(res => {
+          if (res && res.status === 200) {
+            const clone = res.clone();
+            caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+          }
+          return res;
+        })
+        .catch(() => {
+          if (e.request.destination === "document") {
+            return caches.match("/index.html");
+          }
+        });
+    })
+  );
+});
